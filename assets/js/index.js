4c533b33ab1752aeb4d6561d849d184d52413606
khushpatel00 <patelxkhush@gmail.com>
2025-06-12 14:45:28 +0530

    iFrame added to single element

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,11 @@
 const headerElement = document.querySelector('header'); function updateHeaderBackground() { const headerRect = headerElement.getBoundingClientRect(); if (headerRect.top <= 20) { headerElement.classList.add('headerGlass'); headerElement.classList.remove('headerTransparent'); } else { headerElement.classList.add('headerTransparent'); headerElement.classList.remove('headerGlass'); } } window.addEventListener('scroll', updateHeaderBackground); document.addEventListener('DOMContentLoaded', updateHeaderBackground);
+
+const iFrameOpen = function(){
+    iFrameElement = document.getElementById("openSiteIframe");
+    iFrameElementCoffee = document.getElementById("WebPreviewCoffee");
+    iFrameElement.onclick = function(){
+        iFrameElement.classList.remove("col-lg-6","col-10");
+        iFrameElement.classList.add("col-lg-10","col-10");
+        iFrameElementCoffee.classList.remove("d-none");
+    }; 
+};
