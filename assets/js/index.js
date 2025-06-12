7a0dac275e4d9fdc2bda0fce3c6a826d8a35bc5e
Khush Patel <patelxkhush@gmail.com>
2025-06-12 15:17:44 +0530

    fix: Iframe Opened onclick + added Transition

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,9 +3,23 @@ const headerElement = document.querySelector('header'); function updateHeaderBac
 const iFrameOpen = function(){
     iFrameElement = document.getElementById("openSiteIframe");
     iFrameElementCoffee = document.getElementById("WebPreviewCoffee");
+    iFrameImageCoffee = document.getElementById("coffeeImageIframe");
+    // revertIFrame = document.getElementsByClassName("revertIFrame");
     iFrameElement.onclick = function(){
-        iFrameElement.classList.remove("col-lg-6","col-10");
-        iFrameElement.classList.add("col-lg-10","col-10");
-        iFrameElementCoffee.classList.remove("d-none");
+        iFrameElement.classList.remove("col-lg-6","col-12");
+        iFrameElement.classList.add("col-10");
+        iFrameElementCoffee.classList.remove("h-0");
+        iFrameElementCoffee.classList.add("h-100");
+        iFrameImageCoffee.classList.add("d-none");
+        // revertIFrame.classList.add("d-flex");
+        // revertIFrame.classList.remove("d-none");
     }; 
+    // revertIFrame.onclick = function(){
+    //     iFrameElement.classList.add("col-lg-6","col-10");
+    //     iFrameElement.classList.remove("col-lg-10","col-10");
+    //     iFrameElementCoffee.classList.add("d-none");
+    //     revertIFrame.classList.remove("d-flex");
+    //     revertIFrame.classList.add("d-none");
+    // }
 };
+document.addEventListener('DOMContentLoaded', iFrameOpen);
